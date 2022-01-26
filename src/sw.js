d85d8f9b142c172ed6d1df61936b8da80569ab40
blcyzycc <18202762822@163.com>
2022-01-26 22:54:44 +0800

    使用 ES5 的语法改写，增加兼容可能

--- src/sw.js
+++ src/sw.js
@@ -4,18 +4,18 @@
  * */
 
 // 缓存区名称，打包时传入，值为字符串
-const CACHE_NAME = '@@CACHE_NAME@@'
+var CACHE_NAME = '@@CACHE_NAME@@'
 
 // 需要离线缓存的文件，在参与打包时会替换为形如 ['index.html', 'js/index.js'] 的数组
-const CACHE_FILES = '@@CACHE_FILES@@'
+var CACHE_FILES = '@@CACHE_FILES@@'
 
 
 /*
 install 事件是 SW 触发的第一个事件，并且仅触发一次。
 installEvent.waitUntil()接收一个 Promise 参数，用它来表示 SW 安装的成功与否。
 */
-this.addEventListener('install', (event) => {
-  console.log('Service Worker install');
+this.addEventListener('install', function (event) {
+  console.log('Service Worker install')
 
   // 调试时跳过等待过程
   // self.skipWaiting();
@@ -24,7 +24,7 @@ this.addEventListener('install', (event) => {
     caches.open(CACHE_NAME).then(function (cache) {
       // 以下路径的文件会进行缓存
       return cache.addAll(CACHE_FILES)
-    }).catch(err => {
+    }).catch(function (err) {
       console.log(err)
     })
   );
@@ -34,10 +34,12 @@ this.addEventListener('install', (event) => {
 self.addEventListener('activate', function (event) {
   event.waitUntil(
     // 遍历所有 caches 中缓存的 keys 值
-    caches.keys().then(cacheNames => {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
         // CACHE_NAME 更新，删除以前的缓存区文件
-        cacheNames.filter(cacheName => cacheName != CACHE_NAME).map(cacheName => {
+        cacheNames.filter(function (cacheName) {
+          return cacheName != CACHE_NAME
+        }).map(function (cacheName) {
           return caches.delete(cacheName)
         })
       );
@@ -47,7 +49,7 @@ self.addEventListener('activate', function (event) {
 
 /* 拦截浏览器发出的的请求文件申请，根据情况进行返回 */
 self.addEventListener('fetch', function (event) {
-  let SW_NO_CACHE = event.request.headers.get('SW_NO_CACHE')
+  var SW_NO_CACHE = event.request.headers.get('SW_NO_CACHE')
 
   event.respondWith(
     caches.match(event.request).then(function (response) {
@@ -56,14 +58,7 @@ self.addEventListener('fetch', function (event) {
         return response
       } else {
         // console.log('没有缓存，返回网络文件', event.request.url);
-        let set = {}
-
-        // 离线缓存的跨域资源
-        if (CACHE_FILES.includes(event.request.url) && !event.request.url.includes(location.hostname)) {
-          set.mode = 'cors'
-        }
-
-        return fetch(event.request,)
+        return fetch(event.request)
       }
     })
   );
--- src/swLink.js
+++ src/swLink.js
@@ -3,12 +3,12 @@
 // 注册 Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
-    navigator.serviceWorker.register('@@SW_JS_PATH@@', { scope: './' }).then(reg => {
+    navigator.serviceWorker.register('@@SW_JS_PATH@@', { scope: './' }).then(function (reg) {
       console.log('Service Worker 成功');
 
       // 获取当前页面文件流，再根据 CACHE_HASH 判断页面是否已经更新
       // 如果页面已更新，则刷新 sw 和 页面
-      let headers = new Headers()
+      var headers = new Headers()
 
       headers.append('SW_NO_CACHE', 'true') // 设置请求头，用于 fetch 请求拦截判断，从网络请求文件，不取缓存
       headers.append('Pragma', 'no-cache')
@@ -17,16 +17,18 @@ if ('serviceWorker' in navigator) {
 
       fetch('@@INDEX_HTML_PATH@@', {
         headers: headers,
-      }).then(res => res.blob()).then(blob => {
-        let render = new FileReader()
+      }).then(function (res) {
+        return res.blob()
+      }).then(function (blob) {
+        var render = new FileReader()
         render.readAsText(blob, 'utf8')
 
-        render.onload = () => {
-          let r = new RegExp('@' + '@CACHE_HASH=(.*)@' + '@')
-          let match = render.result.match(r)
+        render.onload = function () {
+          var r = new RegExp('@' + '@CACHE_HASH=(.*)@' + '@')
+          var match = render.result.match(r)
 
           if (match && match[1]) {
-            let hash = match[1]
+            var hash = match[1]
 
             if (hash !== localStorage.getItem('CACHE_HASH')) {
               localStorage.setItem('CACHE_HASH', hash)
@@ -36,7 +38,7 @@ if ('serviceWorker' in navigator) {
           }
         }
       })
-    }).catch(err => {
+    }).catch(function (err) {
       // 注册失败:
       console.log('Service Worker 注册失败', err)
     });

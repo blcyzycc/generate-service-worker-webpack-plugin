7051ee4f613d46c4b94bb122d33d2826c3e7b8ae
blcyzycc <18202762822@163.com>
2022-03-16 19:29:30 +0800

    防止用户某些操作导致页面重复刷新

--- src/swLink.js
+++ src/swLink.js
@@ -1,51 +1,71 @@
 '@@CACHE_HASH@@';
 
-// 注册 Service Worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('@@SW_JS_PATH@@', { scope: './' }).then(function (reg) {
-      console.log('Service Worker 成功');
+(function () {
+  // 注册 Service Worker
+  if ('serviceWorker' in navigator) {
+    var clear = localStorage.clear
 
-      // 获取当前页面文件流，再根据 CACHE_HASH 判断页面是否已经更新
-      // 如果页面已更新，则刷新 sw 和 页面
-      var headers = new Headers()
+    // 改写 localStorage.clear 方法，防止用户进入应用执行 clear 导致 CACHE_HASH 丢失，重复刷新页面
+    localStorage.clear = function () {
+      var HASH = localStorage.getItem('CACHE_HASH')
+      var TIME = localStorage.getItem('CACHE_HASH_TIME')
+      clear.call(localStorage)
+      localStorage.setItem('CACHE_HASH', HASH)
+      localStorage.setItem('CACHE_HASH_TIME', TIME)
+    }
 
-      headers.append('SW_NO_CACHE', 'true') // 设置请求头，用于 fetch 请求拦截判断，从网络请求文件，不取缓存
-      headers.append('Pragma', 'no-cache')
-      headers.append('Cache-Control', 'no-cache')
-      headers.append('Expires', '0') // 过期时间为 0 表示立即过期
+    window.addEventListener('load', function () {
+      navigator.serviceWorker.register('@@SW_JS_PATH@@', { scope: './' }).then(function (reg) {
+        console.log('Service Worker 成功');
 
-      fetch('@@INDEX_HTML_PATH@@', {
-        headers: headers,
-      }).then(function (res) {
-        return res.blob()
-      }).then(function (blob) {
-        var render = new FileReader()
-        render.readAsText(blob, 'utf8')
+        // 获取当前页面文件流，再根据 CACHE_HASH 判断页面是否已经更新
+        // 如果页面已更新，则刷新 sw 和 页面
+        var headers = new Headers()
 
-        render.onload = function () {
-          var r = new RegExp('@' + '@CACHE_HASH=(.*)@' + '@')
-          var match = render.result.match(r)
+        headers.append('SW_NO_CACHE', 'true') // 设置请求头，用于 fetch 请求拦截判断，从网络请求文件，不取缓存
+        headers.append('Pragma', 'no-cache')
+        headers.append('Cache-Control', 'no-cache')
+        headers.append('Expires', '0') // 过期时间为 0 表示立即过期
 
-          if (match && match[1]) {
-            var hash = match[1]
+        fetch('@@INDEX_HTML_PATH@@', {
+          headers: headers,
+        }).then(function (res) {
+          return res.blob()
+        }).then(function (blob) {
+          var render = new FileReader()
+          render.readAsText(blob, 'utf8')
 
-            if (hash !== localStorage.getItem('CACHE_HASH')) {
-              localStorage.setItem('CACHE_HASH', hash)
+          render.onload = function () {
+            var r = new RegExp('@' + '@CACHE_HASH=(.*)@' + '@')
+            var match = render.result.match(r)
+
+            // 一分钟内不重复刷新
+            if (Date.now() - localStorage.getItem('CACHE_HASH_TIME') < 3600000) {
+              return
+            }
+
+            if (match && match[1]) {
+              var hash = match[1]
+
+              if (hash !== localStorage.getItem('CACHE_HASH')) {
+                localStorage.setItem('CACHE_HASH_TIME', Date.now())
+                localStorage.setItem('CACHE_HASH', hash)
+                reg.unregister()
+                window.location.reload()
+              }
+            }
+            else {
+              localStorage.setItem('CACHE_HASH_TIME', Date.now())
+              localStorage.removeItem('CACHE_HASH')
               reg.unregister()
               window.location.reload()
             }
           }
-          else {
-            localStorage.removeItem('CACHE_HASH')
-            reg.unregister()
-            window.location.reload()
-          }
-        }
-      })
-    }).catch(function (err) {
-      // 注册失败:
-      console.log('Service Worker 注册失败', err)
+        })
+      }).catch(function (err) {
+        // 注册失败:
+        console.log('Service Worker 注册失败', err)
+      });
     });
-  });
-}
+  }
+})();
